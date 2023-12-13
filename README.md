99a153a096192354260655694a78f66e8572fe95
haiphamhoang <33831192+haiphamhoang@users.noreply.github.com>
2023-12-13 10:48:49 +0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# vps-basic-secure
+# What does this script do?
+Supported distros are Ubuntu, Debian.
+
+- Automatically generates and configures SSH authentication keys.
+- Changes the SSH login port.
+- Installs [Hextrixtool](https://hetrixtools.com/) monitoring.
+- Set timezone to Asia/Bangkok
+
+# How to use?
+
+```bash
+wget -O  secure-basic.sh https://raw.githubusercontent.com/haiphamhoang/vps-basic-secure/main/secure-basic.sh
+chmod +x secure-basic.sh
+./secure-basic.sh
+```
+
---
+++ secure-basic.sh
@@ -0,0 +1,261 @@
+#!/bin/bash
+#
+# https://github.com
+#
+# Copyright (c) 2022 haiphamhoang. Released under the MIT License.
+
+# Visual text settings
+RED="\e[31m"
+GREEN="\e[32m"
+GRAY="\e[37m"
+YELLOW="\e[93m"
+
+REDB="\e[41m"
+GREENB="\e[42m"
+GRAYB="\e[47m"
+ENDCOLOR="\e[0m"
+# Detect Debian users running the script with "sh" instead of bash
+if readlink /proc/$$/exe | grep -q "dash"; then
+	echo 'This installer needs to be run with "bash", not "sh".'
+	exit
+fi
+
+# Discard stdin. Needed when running from an one-liner which includes a newline
+read -N 999999 -t 0.001
+
+# Detect OpenVZ 6
+if [[ $(uname -r | cut -d "." -f 1) -eq 2 ]]; then
+	echo "The system is running an old kernel, which is incompatible with this installer."
+	exit
+fi
+
+# Detect OS
+# $os_version variables aren't always in use, but are kept here for convenience
+if grep -qs "ubuntu" /etc/os-release; then
+	os="ubuntu"
+	os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
+elif [[ -e /etc/debian_version ]]; then
+	os="debian"
+ 	os_version=$(grep -oE '[0-9]+' /etc/debian_version | head -1)
+# elif [[ -e /etc/almalinux-release || -e /etc/rocky-release || -e /etc/centos-release ]]; then
+# 	os="centos"
+# 	os_version=$(grep -shoE '[0-9]+' /etc/almalinux-release /etc/rocky-release /etc/centos-release | head -1)
+# elif [[ -e /etc/fedora-release ]]; then
+# 	os="fedora"
+# 	os_version=$(grep -oE '[0-9]+' /etc/fedora-release | head -1)
+else
+	echo "This installer seems to be running on an unsupported distribution.
+Supported distros are Ubuntu, Debian."
+	exit
+fi
+
+if [[ "$os" == "ubuntu" && "$os_version" -lt 1804 ]]; then
+	echo "Ubuntu 18.04 or higher is required to use this installer.
+This version of Ubuntu is too old and unsupported."
+	exit
+fi
+
+if [[ "$os" == "debian" ]]; then
+	if grep -q '/sid' /etc/debian_version; then
+		echo "Debian Testing and Debian Unstable are unsupported by this installer."
+		exit
+	fi
+	if [[ "$os_version" -lt 10 ]]; then
+		echo "Debian 10 or higher is required to use this installer.
+This version of Debian is too old and unsupported."
+		exit
+	fi
+fi
+
+if [[ "$os" == "centos" && "$os_version" -lt 7 ]]; then
+	echo "CentOS 7 or higher is required to use this installer.
+This version of CentOS is too old and unsupported."
+	exit
+fi
+
+
+
+# Root check
+if [[ "$EUID" -ne 0 ]]; then
+	echo -e "${RED}Sorry, you need to run this as root${ENDCOLOR}"
+	exit 1
+fi
+
+
+set -e
+
+main() {
+
+    # Default option
+    DOCKERINSTALL=false
+    HEXTRIXTOOL=false
+    SSHAUTHKEY=false
+    SSHPORT=1122
+    
+
+    echo "Add/update SSH authorized_keys [ed25519 key] (y/n):"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) 
+                SSHAUTHKEY=true
+                break
+                ;;
+            No )
+                break;;
+        esac
+    done
+
+    
+	echo "Choose your SSH Port [1122]: " 
+    read -p "Note: SSH port will not update, if it already set." sshport
+	until [[ -z "$sshport" || "$sshport" =~ ^[0-9]+$ && "$sshport" -le 65535 ]]; do
+		echo "$sshport: invalid port."
+		read -p "Port [1122]: " sshport
+	done
+	[[ -z "$sshport" ]] && sshport="1122"
+    SSHPORT=$sshport
+
+    echo "Do you want install HEXTRIXTOOL monitor? (y/n):"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) 
+                read -p "Input your monitor id: " hextrixtool_id
+                HEXTRIXTOOL=$hextrixtool_id
+                break
+                ;;
+            No ) 
+                break;;
+        esac
+    done
+
+    # Docker
+    echo "Do you want install or update Docker (y/n):"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) 
+                DOCKERINSTALL=true
+                break
+                ;;
+            No )
+                break;;
+        esac
+    done
+
+    # 
+    # Script Run begin
+
+    #read -p "Your vps location for apt server (ex: jp, us, sg...): " input_server_location
+    # if [  -n "$(uname -a | grep Ubuntu)" ]; then
+    #     # select the fastest apt mirror server
+    #     local input_server_location=$(hostname | cut -d'-' -f2)
+    #     if [ ${#input_server_location} -eq 2 ]; then 
+    #         echo "Change apt server to:" $input_server_location
+    #         sed -i -e "s/http:\/\/[a-z]\{2\}.archive/http:\/\/$input_server_location.archive/" /etc/apt/sources.list
+    #     else 
+    #         echo "Keep apt server."
+    #     fi
+    # fi
+    
+    
+    apt update -y && apt upgrade -y
+    apt install wget curl putty-tools -y
+    timedatectl set-timezone Asia/Bangkok
+
+
+    if [ "$HEXTRIXTOOL" = false ]; then
+      echo "No install Hextrixtool"
+    else
+      wget https://raw.githubusercontent.com/hetrixtools/agent/master/hetrixtools_install.sh && bash hetrixtools_install.sh $HEXTRIXTOOL 0 0 0 0 0 0
+    fi
+
+
+    if [ "$DOCKERINSTALL" = true ]; then
+        echo "Install docker......"
+        apt remove docker.io docker-doc docker-compose podman-docker containerd runc -y
+        apt-get install ca-certificates curl gnupg lsb-release -y
+
+        install -m 0755 -d /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/$(. /etc/os-release && echo "$ID")/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        chmod a+r /etc/apt/keyrings/docker.gpg
+
+        echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$(. /etc/os-release && echo "$ID") "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+        
+        apt-get update -y
+        apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+        systemctl enable docker
+        echo "Docker installed successfully"
+        
+        # Waiting for Docker to start..."
+        sleep 3
+        #curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+
+        docker --version
+        docker compose version
+    fi
+
+    # ssh-key update
+    if [ "$SSHAUTHKEY" = true ]; then
+        if [[ -e "/root/.ssh/authorized_keys" ]]; then
+            echo "Replace authorized_keys file..."
+            sshkey_gen_file
+            edit_ssh_config
+        else
+            # make sure ~/.ssh exists
+            mkdir -p /root/.ssh
+            echo "Add authorized_keys."
+            sshkey_gen_file
+            edit_ssh_config
+        fi
+
+        echo "Success update authorized_keys.";
+        echo "Private key file will be located at: $(pwd)/gensshkey."
+    fi
+
+    # remove file after completed.
+    # cd .. && rm -rf -- linux-shell-script
+    echo "Completed."
+    echo "Remember to reboot your system and delete the private key file located at $(pwd)/gensshkey after saving it!"
+    
+}
+
+
+
+
+#######################################
+# Helper function
+#######################################
+sshkey_gen_file() {
+    echo "Generate sshkey file, make sure you save private key after script is done...."
+    mkdir -p gensshkey
+    ssh-keygen -o -a 256 -t ed25519 -f ./gensshkey/key_file -C "root@$(hostname)-$(date +'%d-%m-%Y')"
+    puttygen ./gensshkey/key_file -O private -o ./gensshkey/putty_private_key.ppk
+    cp ./gensshkey/key_file.pub /root/.ssh/authorized_keys
+    chmod 600 /root/.ssh/authorized_keys
+}
+
+edit_ssh_config() {
+    # edit sshd_config
+    sed -i "s/#Port 22/Port $SSHPORT/" /etc/ssh/sshd_config
+    sed -i 's/#StrictModes.*/StrictModes yes/' /etc/ssh/sshd_config
+    sed -i 's/#MaxAuthTries.*/MaxAuthTries 3/' /etc/ssh/sshd_config
+    sed -i 's/PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+    sed -i 's/#PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+
+    # make sure sshd_config is valid
+    sshd -t
+
+    # restart ssh or sshd depending of the distro
+    service ssh restart ; service sshd restart
+}
+
+#######################################
+# get last release tag version
+# Returns:
+#   string
+#######################################
+compose_release() {
+  curl --silent "https://api.github.com/repos/docker/compose/releases/latest" |
+  grep -Po '"tag_name": "\K.*?(?=")'
+}
+
+main

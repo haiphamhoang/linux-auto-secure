4b7e91fe327f9d884199843b049a472813aaa8ff
haiphamhoang <33831192+haiphamhoang@users.noreply.github.com>
2025-01-03 19:27:21 +0700

    Refactor, improve code

--- secure-basic.sh
+++ secure-basic.sh
@@ -26,35 +26,75 @@ check_requirements() {
     if readlink /proc/$$/exe | grep -q "dash"; then
         echo 'This installer needs to be run with "bash", not "sh".'
         exit 1
-    }
+    fi
 
     # Check if root
     if [[ "$EUID" -ne 0 ]]; then
         echo -e "${RED}Sorry, you need to run this as root${ENDCOLOR}"
         exit 1
-    }
+    fi
 
     # Check OS compatibility
     check_os_compatibility
 }
 
 check_os_compatibility() {
+    # Discard stdin. Needed when running from an one-liner which includes a newline
+    read -N 999999 -t 0.001
+
+    # Detect OpenVZ 6
+    if [[ $(uname -r | cut -d "." -f 1) -eq 2 ]]; then
+        echo "The system is running an old kernel, which is incompatible with this installer."
+        exit
+    fi
+
+    # Detect OS
+    # $os_version variables aren't always in use, but are kept here for convenience
     if grep -qs "ubuntu" /etc/os-release; then
         os="ubuntu"
         os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
-        [[ "$os_version" -lt 1804 ]] && {
-            echo "Ubuntu 18.04 or higher is required."
-            exit 1
-        }
     elif [[ -e /etc/debian_version ]]; then
         os="debian"
         os_version=$(grep -oE '[0-9]+' /etc/debian_version | head -1)
-        if grep -q '/sid' /etc/debian_version || [[ "$os_version" -lt 10 ]]; then
-            echo "Debian 10 or higher is required. Testing/Unstable not supported."
-            exit 1
-        }
+    # elif [[ -e /etc/almalinux-release || -e /etc/rocky-release || -e /etc/centos-release ]]; then
+    # 	os="centos"
+    # 	os_version=$(grep -shoE '[0-9]+' /etc/almalinux-release /etc/rocky-release /etc/centos-release | head -1)
+    # elif [[ -e /etc/fedora-release ]]; then
+    # 	os="fedora"
+    # 	os_version=$(grep -oE '[0-9]+' /etc/fedora-release | head -1)
     else
-        echo "Supported distributions: Ubuntu, Debian"
+        echo "This installer seems to be running on an unsupported distribution.
+    Supported distros are Ubuntu, Debian."
+        exit
+    fi
+
+    if [[ "$os" == "ubuntu" && "$os_version" -lt 1804 ]]; then
+        echo "Ubuntu 18.04 or higher is required to use this installer.
+    This version of Ubuntu is too old and unsupported."
+        exit
+    fi
+
+    if [[ "$os" == "debian" ]]; then
+        if grep -q '/sid' /etc/debian_version; then
+            echo "Debian Testing and Debian Unstable are unsupported by this installer."
+            exit
+        fi
+        if [[ "$os_version" -lt 10 ]]; then
+            echo "Debian 10 or higher is required to use this installer.
+    This version of Debian is too old and unsupported."
+            exit
+        fi
+    fi
+
+    if [[ "$os" == "centos" && "$os_version" -lt 7 ]]; then
+        echo "CentOS 7 or higher is required to use this installer.
+    This version of CentOS is too old and unsupported."
+        exit
+    fi
+
+    # Root check
+    if [[ "$EUID" -ne 0 ]]; then
+        echo -e "${RED}Sorry, you need to run this as root${ENDCOLOR}"
         exit 1
     fi
 }
